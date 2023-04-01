784e9a258ca1a7b348febc5dd7cbdfd74d1966d4
Albina Gurung <albinakushu9545@gmail.com>
2023-04-01 12:02:59 -0700

    Home page for admin is made

--- Administrator/AdminDashboard/Home.php
+++ Administrator/AdminDashboard/Home.php
@@ -4,8 +4,11 @@ require_once("../include/Header.php");
 <?php
 require_once("../include/Navigation.php")
 ?>
-<div class="" style="min-height:70vh">
-    Dashboard
+<div  style="min-height:70vh" >
+      <div class="text-center margin-auto">
+    <button type="button" class="btn btn-primary btn-lg">Bookings</button>
+    <button type="button" class="btn btn-success btn-lg ">Rooms</button>
+    </div>
 </div>
 <?php
 require_once("../include/Footer.php");
